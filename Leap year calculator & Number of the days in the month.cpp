8c0bfd6b0e5879ace32f7e0918d8e48f5f2bf211
serhetgokdemir <102549633+serhetgokdemir@users.noreply.github.com>
2022-10-26 22:44:46 +0300

    Update Leap year calculator & Number of the days in the month.cpp

--- Leap year calculator & Number of the days in the month.cpp
+++ Leap year calculator & Number of the days in the month.cpp
@@ -23,30 +23,30 @@ int main()
 			case 0:
 				if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
 				{
-					printf("This is a leap year. And this month takes 31 days.");
+					printf("This is a leap year and this month takes 31 days.");
 				}
 				else if (month == 4 || month == 6 || month == 9 || month == 11)
 				{
-					printf("This is a leap year. And this month takes 30 days.");
+					printf("This is a leap year and this month takes 30 days.");
 				}
 				else
 				{
-					printf("This is a leap year. And this month takes 29 days");
+					printf("This is a leap year and this month takes 29 days");
 				}
 				break;
 			
 			default:
 				if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
 				{
-					printf("This is not a leap year. And this month takes 31 days.");
+					printf("This is not a leap year and this month takes 31 days.");
 				}
 				else if (month == 4 || month == 6 || month == 9 || month == 11)
 				{
-					printf("This is not a leap year. And this month takes 30 days.");
+					printf("This is not a leap year and this month takes 30 days.");
 				}
 				else
 				{
-					printf("This is not a leap year. And this month takes 28 days");
+					printf("This is not a leap year and this month takes 28 days");
 				}
 				break;
 				
@@ -54,7 +54,7 @@ int main()
 	}
 	else
 	{
-		printf("It's a invalid month number.");
+		printf("It's an invalid month number.");
 	}		
 	return 0;
 	
